94ead128305f46018b94d954d1cc04c2e005f291
Steve S. <J294862@tafe.wa.edu.au>
2025-05-15 01:23:32 +0800

    ROLLEDBACK

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -97,7 +97,8 @@ const ChatContainer = () => {
                 <div className="chat-header mb-1">
                   <time className="text-xs opacity-50 ml-1">
                     {formatMessageTime(message.createdAt)}
-                    {!isCurrentUser && " " + authUser.userName}
+                    {!isCurrentUser && " " + message.senderId.userName}
+                    {/* {!isCurrentUser && " " + authUser.userName} */}
                   </time>
                 </div>
                 <div className="chat-bubble flex flex-col">
