6ab127c086d60eec0968d9cf61c6f8b4df280967
Steve S. <J294862@tafe.wa.edu.au>
2025-05-15 11:57:34 +0800

    Code cleanup

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -21,6 +21,7 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId
 
   if (userId) userSocketMap[userId] = socket.id
+  console.log("A user connected", socket.id)
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap))
 
